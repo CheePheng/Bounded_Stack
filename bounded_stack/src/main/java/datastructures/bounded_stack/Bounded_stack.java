81d9a2ffa93274f18c4803a085f098c6c95db50c
D00216500 <D00216500@P1158-PC06.ad.dkit.ie>
2023-03-06 12:54:27 +0000

    Added basic function for stack, missing pop() and addfirst

--- bounded_stack/src/main/java/datastructures/bounded_stack/Bounded_stack.java
+++ bounded_stack/src/main/java/datastructures/bounded_stack/Bounded_stack.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package datastructures.bounded_stack;
 
 /**
@@ -11,7 +10,56 @@ package datastructures.bounded_stack;
  */
 public class Bounded_stack {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private Node first;
+
+    private int numElements;
+
+    public Bounded_stack() {
+        first = null;
+        numElements = 0;
+    }
+
+    public int size() {
+        return numElements;
+    }
+
+    public boolean isEmpty() {
+        //return first == null;
+        return numElements == 0;
+    }
+
+    public Node peek() {
+        if (numElements <= 0 || first == null) {
+            throw new IndexOutOfBoundsException("No data");
+        } else {
+
+            return first;
+        }
+
+    }
+    
+  
+
+    public void push(String value) {
+        Node newNode = new Node(value);
+        newNode.next = first;
+        first = newNode;
+        numElements++;
+    }
+
+    private static class Node {
+
+        private String data;
+        private Node next;
+
+        public Node(String value) {
+            this.data = value;
+            this.next = null;
+        }
+
+        public boolean hasNext() {
+            return next != null;
+        }
+
     }
 }
