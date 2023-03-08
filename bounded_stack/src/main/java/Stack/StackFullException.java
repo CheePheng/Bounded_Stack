46441624015bed0b035431f0c1911f8ba2b9bdda
D00216500 <D00216500@P1159-PC19.ad.dkit.ie>
2023-03-08 12:04:54 +0000

    added all basic stack functionality

---
+++ bounded_stack/src/main/java/Stack/StackFullException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Stack;
+
+/**
+ *
+ * @author D00216500
+ */
+public class StackFullException extends RuntimeException{
+
+    public StackFullException() {
+    }
+
+    public StackFullException(String message) {
+        super(message);
+    }
+    
+    
+}
